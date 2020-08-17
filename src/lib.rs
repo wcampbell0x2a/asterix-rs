b28485326eb50035d0215f3abd5c2515f87bbf58
wcampbell <wcampbell1995@gmail.com>
2020-08-16 20:40:11 -0400

    Parse multiple messages correctly
    Signed-off-by: wcampbell <wcampbell1995@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -21,9 +21,51 @@ pub struct AsterixPacket {
     pub category: u8,
     #[deku(bytes = "2")]
     pub length: u16,
-    // TODO Update to Vec<T> till length is read
-    #[deku(ctx = "*category")]
-    pub message: AsterixMessage,
+    #[deku(
+        reader = "Self::read_messages(rest, *category, *length)",
+        writer = "Self::write_messages(&self.messages, *category)"
+    )]
+    pub messages: Vec<AsterixMessage>,
+}
+
+impl AsterixPacket {
+    /// Parse as messages until the length of bits read matches `length`
+    fn read_messages(
+        rest: &BitSlice<Msb0, u8>,
+        category: u8,
+        length: u16,
+    ) -> Result<(&BitSlice<Msb0, u8>, Vec<AsterixMessage>), DekuError> {
+        let mut inside_rest = rest;
+        let mut messages = vec![];
+
+        // The finish len is the bytes subtracted by the length - 3 (header bytes)
+        let finish_len = (inside_rest.len() / 8) - usize::from(length - 3);
+
+        // loop until the correct number of bytes have been read, then return Vec
+        loop {
+            let (new_rest, value) =
+                DekuRead::read(inside_rest, (deku::ctx::Endian::Big, category))?;
+            let value: AsterixMessage = Result::<_, DekuError>::Ok(value)?;
+            messages.push(value);
+            inside_rest = new_rest;
+            if inside_rest.len() / 8 == finish_len {
+                break;
+            }
+        }
+        Ok((inside_rest, messages))
+    }
+
+    fn write_messages(
+        messages: &Vec<AsterixMessage>,
+        category: u8,
+    ) -> Result<BitVec<Msb0, u8>, DekuError> {
+        let mut acc: BitVec<Msb0, u8> = BitVec::new();
+        for message in messages {
+            let bits = message.write((deku::ctx::Endian::Big, category))?;
+            acc.extend(bits);
+        }
+        Ok(acc)
+    }
 }
 
 #[derive(Debug, PartialEq, DekuRead, DekuWrite)]
